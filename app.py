0af6242dc9f2f59ca331c8c86421ff6bcb24d3ad
apitodolist <163763049+apitodolist@users.noreply.github.com>
2024-03-17 20:59:39 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,35 +1,74 @@
-from fastapi import FastAPI
-from fastapi.responses import FileResponse
-
+#API Functions
+#--------------------------------------
+#Imports
+import pymongo
 from pydantic import BaseModel
+from fastapi import FastAPI
+from bson import ObjectId
 
 app = FastAPI()
 
 
-class Item(BaseModel):
-    item_id: int
+#-------------usersDataBase------------#
 
+#Conexion
+client = pymongo.MongoClient('mongodb+srv://apitodolist:abacux64@cluster0.6dzr981.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
+basedD = client['ToDoList']
+collection = basedD['tasks']
+#Esquema
+class Task(BaseModel):
+   id: str
+   task: str
+   isDone: str
 
+"""{
+  "id": "id",
+  "task": "userName",
+  "isDone": "password"
+}"""
+
+def userSchema(task) -> dict:
+   print(task)
+   return {"_id": str(task["id"]),
+            "task": task["task"],
+            "isDone": task["isDone"]}
+
+#Funciones
+#find
 @app.get("/")
-async def root():
-    return {"message": "Hello World"}
+async def read_root():
+   return {"API": "ToDoList"}
 
+@app.get("/tasks/")
+async def getTasks():
+      data = {}
+      for u in collection.find({}):
+          data[str(u["_id"])] = u
+      return data
 
-@app.get('/favicon.ico', include_in_schema=False)
-async def favicon():
-    return FileResponse('favicon.ico')
+@app.get("/one-task/{itemId}")
+async def getOneTask(itemId:str):
+      data = collection.find_one({itemId})
+      return data
 
+@app.post("/post-task/")
+async def postTask(modelo:Task):
+   dict_modelo = dict(modelo)
+   idValue= dict_modelo["id"]
+   dict_modelo.pop("id")
+   dict_modelo["_id"]=idValue
 
-@app.get("/item/{item_id}")
-async def read_item(item_id: int):
-    return {"item_id": item_id}
+   collection.insert_one(dict_modelo)
 
+@app.put("/put-task/{itemId}")
+async def putTask(itemId:str, modelo:Task):
+    dict_modelo = dict(modelo)
+    idValue= dict_modelo["id"]
+    dict_modelo.pop("id")
+    dict_modelo["_id"]=idValue
 
-@app.get("/items/")
-async def list_items():
-    return [{"item_id": 1, "name": "Foo"}, {"item_id": 2, "name": "Bar"}]
+    collection.find_one_and_replace({"_id":itemId},dict_modelo)
 
-
-@app.post("/items/")
-async def create_item(item: Item):
-    return item
+@app.delete("/delete-task/{itemId}")
+async def deleteTask(itemId:str):
+  collection.delete_one({"_id":itemId})
